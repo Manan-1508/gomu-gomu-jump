74ab7bae1b9f64767b4c8c9e669177410769c64f
Manan-108 <manansangwan15@gmail.com>
2024-09-20 23:15:07 +0530

    3)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from pygame.image import load
 from editor import Editor
 class Main:
 	def __init__(self):
@@ -7,6 +8,10 @@ class Main:
 		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 		self.clock = pygame.time.Clock()
 		self.editor = Editor()
+		# cursor 
+		surf = load(r'C:\Users\Manan\OneDrive\Desktop\gomu-gomu\images\graphics\cursors\mouse.png').convert_alpha()
+		cursor = pygame.cursors.Cursor((0,0), surf)
+		pygame.mouse.set_cursor(cursor)
 	def run(self):
 		while True:
 			dt = self.clock.tick() / 1000
